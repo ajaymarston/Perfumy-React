2b76aabb80adc4e371b2347db92d4d1ae62e554c
ajaymarston <ajaymarston01@gmail.com>
2026-02-08 12:36:59 +0530

    Update Project

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar(){
+ return(
+  <nav>
+    <h1>Perfumy</h1>
+    <ul>
+      <li><a>Home</a></li>
+      <li><a>Products</a></li>
+      <li><a></a></li>
+      <li><a>Contact</a></li>
+    </ul>
+  </nav>
+ )
+}
+export default Navbar
