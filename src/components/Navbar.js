dba557226616087b79c5e0b1e95d32a6c18f849a
ajaymarston <ajaymarston01@gmail.com>
2026-02-08 12:56:27 +0530

    Update Project 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ function Navbar(){
   <nav>
     <h1>Perfumy</h1>
     <ul>
-      <li><a>Home</a></li>
-      <li><a>Products</a></li>
-      <li><a></a></li>
-      <li><a>Contact</a></li>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#products">Products</a></li>
+    <li><a href="#contact">Contact</a></li>
     </ul>
   </nav>
  )
